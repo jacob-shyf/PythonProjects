d8436b88d32240f0046f0a37185aec0f47b41aec
Jacob Scheiffler <jacob.scheiffler@colorado.edu>
2023-12-06 20:49:42 -0700

    Finishing basic functionality

--- WorkoutArchives/workouts.py
+++ WorkoutArchives/workouts.py
@@ -6,10 +6,31 @@ class HIT:
         self.hit = 'hit.txt'
         self.prev_hit = 'prev_hit.txt'
         self.archive = 'WorkoutArchives/HIT/'
+        self.hit_wo = 'Nut up or shut up!'
         self.blacklist = []
 
     def selectHIT(self):
-        return 'HIT Workout'
+        # Read CYOA workout options
+        hit_file = open(self.archive + self.hit, 'r')
+        hit_options = hit_file.readlines()
+        hit_file.close()
+
+        # Read previous workout selection
+        prev_hit_file = open(self.archive + self.prev_hit, 'r')
+        prev_wo = prev_hit_file.readline()
+        prev_hit_file.close()
+        
+        # Generate a random choice that isn't blacklisted or the previous choice
+        self.hit_wo = random.choice(hit_options)
+        while self.hit_wo == prev_wo or self.blacklist.count(self.hit_wo):
+            self.hit_wo = random.choice(hit_options)
+
+        # Save workout selection
+        prev_hit_file = open(self.archive + self.prev_hit, 'w')
+        prev_hit_file.write(self.hit_wo)
+        prev_hit_file.close()
+
+        self.hit_wo = self.hit_wo.strip()
 
 # Choose-Your-Own-Adventure (Activitiy Ideas)
 class CYOA:
@@ -17,6 +38,7 @@ class CYOA:
         self.cyoa = 'cyoa.txt'
         self.prev_cyoa = 'prev_cyoa.txt'
         self.archive = 'WorkoutArchives/CYOA/'
+        self.cyoa_wo = 'Fly away Stanley, be free!'
         self.blacklist = ['Lacrosse Workout\n']
 
     def selectCYOA(self):
@@ -27,17 +49,17 @@ class CYOA:
 
         # Read previous workout selection
         prev_cyoa_file = open(self.archive + self.prev_cyoa, 'r')
-        prev_wo = prev_cyoa_file.readlines()
+        prev_wo = prev_cyoa_file.readline()
         prev_cyoa_file.close()
 
         # Generate a random choice that isn't blacklisted or the previous choice
-        cyoa_wo = random.choice(cyoa_options)
-        while cyoa_wo == prev_wo or self.blacklist.count(cyoa_wo):
-            cyoa_wo = random.choice(cyoa_options)
+        self.cyoa_wo = random.choice(cyoa_options)
+        while self.cyoa_wo == prev_wo or self.blacklist.count(self.cyoa_wo):
+            self.cyoa_wo = random.choice(cyoa_options)
 
         # Save workout selection
         prev_cyoa_file = open(self.archive + self.prev_cyoa, 'w')
-        prev_cyoa_file.write(cyoa_wo)
+        prev_cyoa_file.write(self.cyoa_wo)
         prev_cyoa_file.close()
 
-        return cyoa_wo.strip()
+        self.cyoa_wo = self.cyoa_wo.strip()
--- wo_gen.py
+++ wo_gen.py
@@ -5,22 +5,17 @@ from WorkoutArchives import workouts
 cyoa = workouts.CYOA() # Choose-Your-Own-Adventure Class
 hit = workouts.HIT()   # High Intensity Training Class
 
-# Set Defaults
-cyoa_selection = 'Fly away Stanley, be free!'
-hit_selection = 'Nut up or shut up!'
-
 # Select CYOA Workout
 cyoa_yn = input("Select Choose-Your-Own-Adventure exercise? (y/n): ")
 if cyoa_yn.lower() == 'y':
-    cyoa_selection = cyoa.selectCYOA()
+    cyoa.selectCYOA()
 
 # Select HIT Workout
 hit_yn = input("Select High Intensity exercise? (y/n): ")
 if hit_yn.lower() == 'y':
-    hit_selection = hit.selectHIT()
-
+    hit.selectHIT()
 
 # Print Results
 print('\n' + 'Your selected workouts for the week are:')
-print('    ' + 'CYOA - ' + cyoa_selection)
-print('    ' + 'HIT  - ' + hit_selection)
+print('    ' + 'CYOA - ' + cyoa.cyoa_wo)
+print('    ' + 'HIT  - ' + hit.hit_wo)
